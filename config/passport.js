631ff883197a800705130b9d22721dc862cabea2
Harshit <virtualharshit@gmail.com>
2024-07-03 00:41:27 +0530

    Done

--- config/passport.js
+++ config/passport.js
@@ -7,7 +7,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: `/auth/google/callback`,
+      callbackURL: `https://gentlereminder.vercel.app/auth/google/callback`,
       scope: ["profile", "email"],
     },
     async (accessToken, refreshToken, profile, done) => {
